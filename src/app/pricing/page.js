78492e4531688bf285020462eeffd6f8322a6d76
somtochukwu <chukwusomto113@gmail.com>
2025-12-01 16:09:11 +0100

    modified my imports

---
+++ src/app/pricing/page.js
@@ -0,0 +1,15 @@
+import PricingHero from "../../component/Pricing/PricingHero";
+import PricingTags from "../../component/Pricing/PricingTags";
+import Faq from "../../component/Pricing/Faq";
+
+export default function Page() {
+  return(
+    <main>
+      <PricingHero />
+      <PricingTags/>
+      <Faq/>
+    </main>
+  ) 
+
+  ;
+}
